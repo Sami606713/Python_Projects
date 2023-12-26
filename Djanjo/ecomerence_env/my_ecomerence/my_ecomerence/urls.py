aab73ff5c0ef3e194c49950fd163c45a6920bcbb
samiullah <sami606713@gmail.com>
2023-12-26 18:42:59 +0500

    adjust the product view page

--- Djanjo/ecomerence_env/my_ecomerence/my_ecomerence/urls.py
+++ Djanjo/ecomerence_env/my_ecomerence/my_ecomerence/urls.py
@@ -20,7 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-  
     # main app urls
     path("product/",include("products.urls")),
     path("",views.index,name="index")
